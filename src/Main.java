f271680f7bb9af2a537564b3e130e3f346ffa98e
linke <linke@duck.com>
2025-05-28 18:04:04 +0300

    created a new thread, completed task 2 and corrected the errors

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         // task 1
+
+        System.out.println("Задача 1");
         String firstName = "Ivan";
         String middleName = "Ivanovich";
         String lastName = "Ivanov";
         String fullName = lastName + " " + firstName + " " + middleName;
-        System.out.println(fullName);
+        System.out.println("Ф.И.О сотрудника " + fullName);
+
+        // task 2
+
+        System.out.println("Задача 2");
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
     }
 }
